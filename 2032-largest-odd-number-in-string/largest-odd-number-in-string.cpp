6f4916342b7aefda40a49ce456fefe2610ea234d
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-03-26 00:24:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.91%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string ans="";
+        int n=num.length();
+        for (int i = n-1;i>=0;i--){
+            int  digit  =num[i]-'0';
+            if (digit%2!=0){
+                return num.substr(0,i+1);
+            }
+            
+        }
+        return "";
+
+    }
+};
