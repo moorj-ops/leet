dc7ee1f24fbb3f572deef7764d44bc9f6b527c06
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-03-28 23:00:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (23.82%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        unordered_map<char,int> mp;
+        for(char ch:s) {
+            mp[ch]++;
+        }
+        for(char ch: t){
+            mp[ch]--;
+        }
+        for(auto it : mp){
+            if(it.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
