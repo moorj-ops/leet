31189b22f798ced177a120e6871548bcbe145ece
Riya Moorjnai <riyamoorjani25@gmail.com>
2026-04-03 23:35:26 +0530

    Time: 6 ms (29.01%) | Memory: 13.8 MB (28.20%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<long long> st;
+        long long result = 0;
+        long long num = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if (c == '(') {
+                st.push(result);
+                st.push(sign);
+
+                result = 0;
+                sign = 1;
+            }
+            else if (c == ')') {
+                result += sign * num;
+                num = 0;
+
+                long long prevSign = st.top(); st.pop();
+                long long prevResult = st.top(); st.pop();
+
+                result = prevResult + prevSign * result;
+            }
+        }
+
+        result += sign * num;
+
+        return (int)result; // final result fits in int
+    }
+};
